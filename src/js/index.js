c1ee1f4624c3b139d8bb1e5ad6a7ce8acc3a7f66
Le Thanh Tam <thanhtam.jane@gmail.com>
2018-02-02 12:01:36 +0100

    Deal second Dealer Card

--- src/js/index.js
+++ src/js/index.js
@@ -39,6 +39,14 @@ function startGame(){
 
 $('#deal-btn').on("click",dealerCards);
 function dealerCards(){
+	dealerCards1();
+	dealerCards2();
+	};
+
+
+
+
+function dealerCards1(){
 	var currentCard = deck[0];
 	//remove 1 card - pop out of deck, add to discarded array
 	var dealerHand = [];
@@ -51,12 +59,31 @@ function dealerCards(){
 	console.log(currentCard);
 	var dealerHand1Class = currentCard.suit + '-' + currentCard.value;
 	console.log(dealerHand1Class);
-	if (dealerHand[0].reversed = false){
+	if (dealerHand[0].reversed == false){
 		$('#dealer-hand1').addClass(dealerHand1Class);
 	} else{
 		$('#dealer-hand1').addClass('revers');
-	}
+	};
+};
 
 
+function dealerCards2(){
+	var currentCard = deck[0];
+	//remove 1 card - pop out of deck, add to discarded array
+	var dealerHand = [];
+	dealerHand.push({
+			'suit': currentCard.suit,
+			'value': currentCard.value,
+			'reversed': false
+		});
+	// Show on dealer's Hand
+	console.log(currentCard);
+	var dealerHand2Class = currentCard.suit + '-' + currentCard.value;
+	console.log(dealerHand2Class);
+	if (dealerHand[0].reversed == false){
+		$('#dealer-hand2').addClass(dealerHand2Class);
+	} else{
+		$('#dealer-hand2').addClass('revers');
+	};
 };
 
